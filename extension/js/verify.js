e9e6ff42cc15b04294bedd6d15ddd138cf98734d
Jay Lee <jay0lee@gmail.com>
2025-03-06 07:05:41 -0500

    Update verify.js

--- extension/js/verify.js
+++ extension/js/verify.js
@@ -67,49 +67,19 @@ var verifyBoxShow=function(){
 		var nameCss='width:195px;text-align:right;padding-right:10px';
 		console.log('challenge type:');
 		console.log(chall.type);
-		if(chall.type=="dns-01"){
-			html.push('Please go to the DNS resolution management of your domain name and add a <i class="i">TXT record</i> for the following subdomain name (a subdomain name can have multiple TXT records at the same time, and the old records can be modified or deleted).</div>');
-			html.push(`<div class="pd FlexBox">
-				<div style="${nameCss}">${'Sub Domain'}:</div>
-				<div class="FlexItem">
-					<input style="width:100%" readonly value="_acme-challenge.${auth.identifier.value}" />
-				</div>
+		html.push('For non-predefined authentication types, please use <i class="i">Key Authorizations (Token+.+Thumbprint)</i> to handle it yourself. <i class="i">Digest</i> is the SHA-256 Base64 value of Key Authorizations.</div>');
+		html.push(`<div class="pd FlexBox">
+			<div style="${nameCss}">Key Authorizations:</div>
+			<div class="FlexItem">
+				<input style="width:100%" readonly value="${chall.authTxt}" />
 			</div>
-			<div class="pd FlexBox">
-				<div style="${nameCss}">${Lang('TXT记录','TXT Record')}:</div>
-				<div class="FlexItem">
-					<input style="width:100%" readonly value="${chall.authTxtSHA256}" />
-				</div>
-			</div>`);
-		}else if(chall.type=="http-01"){
-			html.push(Lang('请在你的网站根目录中创建<i class="i">/.well-known/acme-challenge/</i>目录，目录内创建<i class="i">'+FormatText(chall.token)+'</i>文件，文件内保存下面的文件内容，保存好后<a href="http://'+auth.identifier.value+'/.well-known/acme-challenge/'+FormatText(chall.token)+'" target="_blank">请点此打开此文件URL</a>测试能否正常访问；注意：这个文件URL必须是80端口，并且公网可以访问，否则ACME无法访问到此地址将会验证失败。Windows操作提示：Windows中用<i class="i">.well-known.</i>作为文件夹名称就能创建<i class="i">.well-known</i>文件夹；IIS可能需在此文件夹下的MIME类型中给 <i class="i">.</i> （扩展名就是一个字"."）添加 <i class="i">text/plain</i> 才能正常访问。','Please create the <i class="i">/.well-known/acme-challenge/</i> directory in the root directory of your website, create the <i class="i">'+FormatText(chall.token)+'</i> file in the directory, and save the following file content in the file; After saving, <a href="http://'+auth.identifier.value+'/.well-known/acme-challenge/'+FormatText(chall.token)+'" target="_blank">please click here to open the URL</a> of this file to test whether Normal access; note: the URL of this file must be 80 The port and the public network can be accessed, otherwise ACME cannot access this address and the verification will fail. Windows operation tips: In Windows, you can create a <i class="i">.well-known</i> folder by using <i class="i">.well-known.</i> as the folder name; IIS may need to give <i class="i">.</i> in the MIME type under this folder (the extension is a word ".") Add <i class="i">text/plain</i> for normal access.')+'</div>');
-			html.push(`<div class="pd FlexBox">
-				<div style="${nameCss}">${Lang('文件URL','File URL')}:</div>
-				<div class="FlexItem">
-					<input style="width:100%" readonly value="http://${auth.identifier.value}/.well-known/acme-challenge/${FormatText(chall.token)}" />
-				</div>
+		</div>
+		<div class="pd FlexBox">
+			<div style="${nameCss}">Digest(SHA-256 Base64):</div>
+			<div class="FlexItem">
+				<input style="width:100%" readonly value="${chall.authTxtSHA256Base64}" />
 			</div>
-			<div class="pd FlexBox">
-				<div style="${nameCss}">${Lang('文件内容','File Content')}:</div>
-				<div class="FlexItem">
-					<input style="width:100%" readonly value="${chall.authTxt}" />
-				</div>
-			</div>`);
-		} else {
-			html.push(Lang('非预定义验证类型，请使用<i class="i">Key Authorizations (Token+.+指纹)</i>自行处理，<i class="i">Digest</i>为Key Authorizations的SHA-256 Base64值。','For non-predefined authentication types, please use <i class="i">Key Authorizations (Token+.+Thumbprint)</i> to handle it yourself. <i class="i">Digest</i> is the SHA-256 Base64 value of Key Authorizations.')+'</div>');
-			html.push(`<div class="pd FlexBox">
-				<div style="${nameCss}">Key Authorizations:</div>
-				<div class="FlexItem">
-					<input style="width:100%" readonly value="${chall.authTxt}" />
-				</div>
-			</div>
-			<div class="pd FlexBox">
-				<div style="${nameCss}">Digest(SHA-256 Base64):</div>
-				<div class="FlexItem">
-					<input style="width:100%" readonly value="${chall.authTxtSHA256Base64}" />
-				</div>
-			</div>`);
-		}
+		</div>`);
 		$(".verifyItemBox_"+i0).html(html.join('\n'));
 	});
 	for(var i0=0;i0<domains.length;i0++){
