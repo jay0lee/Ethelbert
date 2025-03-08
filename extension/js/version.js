d81e65916d2fc950fc7539aa9ec9923acb767f10
Jay Lee <jay0lee@gmail.com>
2025-03-08 09:33:07 -0500

    Update version.js

--- extension/js/version.js
+++ extension/js/version.js
@@ -1,5 +1,6 @@
-var manifest = await chrome.runtime.getManifest();
-var Version = manifest.version;
-var ExtName = manifest.name;
-console.log(ExtName + " " + Version);
-console.log("LICENSE: GPL-3.0, https://github.com/xiangyuecn/ACME-HTML-Web-Browser-Client/blob/main/LICENSE");
+chrome.runtime.getManifest(function(manifest) {
+  var ExtVersion = manifest.version;
+  var ExtName = manifest.name;
+  console.log(ExtName + " " + Version);
+  console.log("LICENSE: GPL-3.0, https://github.com/xiangyuecn/ACME-HTML-Web-Browser-Client/blob/main/LICENSE");
+  });
