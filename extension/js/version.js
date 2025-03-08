dd0161bf22bec621bdb9bdd54b4049e58fd8e042
Jay Lee <jay0lee@gmail.com>
2025-03-08 09:28:19 -0500

    Update version.js

--- extension/js/version.js
+++ extension/js/version.js
@@ -1,9 +1,3 @@
-var Version="1.0.240804";
+var manifest = await chrome.runtime.getManifest();
+console.log(manifest.name + " " manifest.version);
 console.log("LICENSE: GPL-3.0, https://github.com/xiangyuecn/ACME-HTML-Web-Browser-Client/blob/main/LICENSE");
-/***********************************
-中英对照翻译主要来自：Chrome自带翻译+百度翻译，由中文翻译成English（作者英文很菜）。
-The Chinese-English translation is mainly from: Chrome comes with translation + Baidu translation, which is translated from Chinese to English (the author's English level is very low)
-
-感谢围观本客户端源码，所有功能的代码都在本页面一个文件内，逻辑上会比较长和比较丑（关键地方或多或少有写注释），如感不适，请转头看看旁边的大长腿，再深呼吸一下继续看下一行~
-Thank you for watching the source code of this client. The code of all functions is in one file on this page, which is logically long and ugly (more or less comments are written in key places). If you feel uncomfortable, please turn your head and look at the Big Long Legs next to you, take another deep breath and continue to look at the next line~
-************************************/
