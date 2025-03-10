699253ca3fcbf1d66d10e1f21fce20520243c129
Jay Lee <jay0lee@gmail.com>
2025-03-10 06:42:25 -0400

    [no ci] Update version.js

--- extension/js/version.js
+++ extension/js/version.js
@@ -3,4 +3,4 @@ var ExtVersion = manifest.version;
 var ExtName = manifest.name;
 document.title = ExtName;
 console.log(ExtName + " " + ExtVersion);
-console.log("LICENSE: GPL-3.0, https://github.com/xiangyuecn/ACME-HTML-Web-Browser-Client/blob/main/LICENSE");
+console.log("LICENSE: GPL-3.0, https://github.com/jay0lee/Ethelbert/blob/main/LICENSE");
